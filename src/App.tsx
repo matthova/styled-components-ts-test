ed4918522935a222b4725277eba69eee1f7a5337
matthova <matthova87@gmail.com>
2020-02-06 22:25:21 -0800

    Basic example with an override on styled-components library default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import baseStyled, {ThemeProvider, ThemedStyledInterface} from 'styled-components';
+
 import logo from './logo.svg';
 import './App.css';
 
+export const theme = {
+    color: {
+        red: '#FFAAAA'
+    },
+};
+
+export type Theme = typeof theme;
+
+export const styled = baseStyled as ThemedStyledInterface<Theme>;
+
+const Foo = styled('div')`
+  background: ${p => p.theme.color.red};
+`;
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Foo>Foo</Foo>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
